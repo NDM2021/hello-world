7ac68c1491a76c73a297b7c0901f378578106563
NDM2021 <60229287+NDM2021@users.noreply.github.com>
2020-02-11 23:34:00 -0600

    Add files via upload
    Added comments to the code such as #Name #Date #What does the code do?
    
    When documenting the code, I made sure that it's accurate and debugged so it can give out desired outcomes from the users

---
+++ NorrisMayesCSS225M4P1.py
@@ -0,0 +1,13 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait?")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt)
+
+# Norris Mayes
+# 2/11/20
+# This program calculate user final wait time
+
